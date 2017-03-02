97bd3235d134e7bcce1e172d54a85d167a080b8c
wjdelaney <wjdelan@gmail.com>
2017-03-02 15:11:54 +0000

    Added initial code for parsing command line input in main.py

--- Assignment3/main.py
+++ Assignment3/main.py
@@ -5,6 +5,9 @@ Created on 28 Feb 2017
 '''
 import sys
 import argparse
+import urllib.request
+import re
+
 def main():    
     # parse the command line arguments to get the input filename
     parser = argparse.ArgumentParser()
@@ -13,25 +16,26 @@ def main():
 
     # now the filename arguments is in args.input
     filename = args.input
-
-    # read_file reads the contents of the buffer into a single string
-    buffer = read_file(filename=filename)
+    fh2 =urllib.request.urlopen(filename)
+    buffer = fh2.read().decode('utf-8')
 
     # break the buffer into lines
     lines = buffer.split('\n')
-
     # the first line is the size of the LED array
     size = int(lines[0])
 
+    print (size)
+    for i, line in enumerate(lines[1:-1]):
+        print (i, line)
     # I construct a class to do the testing
-    tester = LEDTester(size)
+    #tester = LEDTester(size)
 
     # now loop over each line (except the first) and process the commands (turn on/off, switch)
-    for i, line in enumerate(lines[1:]):
-        tester.execute_command(line)
+    #for i, line in enumerate(lines[1:]):
+        #tester.execute_command(line)
 
     # at the end, count the number of lights that are on
-    print("{} {}".format(filename, tester.count_lighting()))
-    return
+   # print("{} {}".format(filename, tester.count_lighting()))
+    #return
 if __name__ == '__main__':
     pass
