2e67d523f9ef1dd30c4d9a72547eeaf7edd9ac35
wjdelaney <wjdelan@gmail.com>
2017-03-03 19:11:50 +0000

    updated setup file

--- Assignment3/setup.py
+++ Assignment3/setup.py
@@ -6,6 +6,6 @@ setup(name="Assignment3",
       url="",
       author="William Delaney",
       author_email="william.delaney1@ucdconnect.ie",
-      licence="GPL3",
+      license="GPL3",
       packages=['Assignment3'],
       entry_points={'console_scripts':['solve_led=Assignment3.main:main']})
