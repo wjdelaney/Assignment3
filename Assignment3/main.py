3f1a69c60bd2e4a937b95c8070f6cf882e5ca023
wjdelaney <wjdelan@gmail.com>
2017-03-03 09:39:23 +0000

    Added code for command functions and ran initial test on assign3.txt file with correct calculation of 400410 lights on.

--- Assignment3/main.py
+++ Assignment3/main.py
@@ -11,18 +11,30 @@ import re
 def filter(x):
     x = int(x)
     x = 0 if (x < 0) else x
-    x = size if (x > size) else x
+    x = size-1 if (x > size) else x
     return x
  
 #Turn on function, change array values to a 1.
 def turn_on(x1,x2,y1,y2):  
-    pass
+    for i in range (x1,x2+1):
+        for j in range (y1,y2+1):
+            array2d[i][j] = 1
+                                
 #Turn off function, change array values to a 0.
 def turn_off(x1,x2,y1,y2):  
-    pass
+    for i in range (x1,x2+1):
+        for j in range (y1,y2+1):
+            array2d[i][j] = 0
+            
 #Switch 0 to 1 OR 1 to 0 in the defined area.
 def switch(x1,x2,y1,y2):  
-    pass
+    for i in range (x1,x2+1):
+        for j in range (y1,y2+1):
+            if array2d[i][j]==1:
+                array2d[i][j]=0
+            else:
+                array2d[i][j]=1
+                
 def main():  
     global size
     global array2d  
@@ -46,21 +58,21 @@ def main():
     array2d = [ [0]*size for _ in range(size) ]
     print (size)
     for i, line in enumerate(lines[1:-1]):
-        print (i, line)
+        #print (i, line)
         # Regex expression to find the command and co-ordinates
         m = re.match(r'(((\D+ \D+)|(\D+)) (.?\d+)(.?)(.?\d+) through (.?\d+)(.?)(.?\d+))',line)
-        print("matched: ", m is not None, m.groups())
+        #print("matched: ", m is not None, m.groups())
         # Is this a valid matched line of input? skip line if false.
         mtest = m is not None
         if (mtest == 'false'):
             continue
         #Assign variables to the command & co-ordinates
         cmd,x1,y1,x2,y2 = m.group(2),m.group(5),m.group(7),m.group(8),m.group(10)
-        filter(x1)
-        filter(x2)
-        filter(y1)
-        filter(y2)
-        
+        x1 = filter(x1)
+        x2 = filter(x2)
+        y1 = filter(y1)
+        y2 = filter(y2)
+        print (x2-x1,y2-y1,mtest)
         #print(cmd,":",x1,y1,x2,y2,mtest)
         # execute
         if cmd == "turn on":
@@ -70,9 +82,10 @@ def main():
         elif cmd == "switch":
             switch(x1,x2,y1,y2)
         else:
+            x1=x1
         # unknown command: do nothing
- 
-    fh2.close()
+    total = sum(x.count(1) for x in array2d)
+    print (total)
     # I construct a class to do the testing
     #tester = LEDTester(size)
 
